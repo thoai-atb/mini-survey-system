0077f9829112f912e2babe2dfe09d215992d17cb
Thoai Ly <lybaothoai@gmail.com>
2021-03-28 21:59:09 +0700

    add logo

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,32 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './Nav.css'
+import logo from "./minisurvey.png"
 
 const Nav = () => {
     return (
         <nav>
           <div className="nav-left">
             <div className="nav-item">
-              <p>Logo</p>
+              <img height="50px" src={logo} alt="logo"></img>
             </div>
             <div className="nav-item">
-              <Link to='/'>
+              <Link style={{textDecoration: 'none'}} to='/'>
                 <p>Home</p>
               </Link>
             </div>
             <div className="nav-item">
-              <Link to='/browse'>
+              <Link style={{textDecoration: 'none'}} to='/browse'>
                 <p>Browse</p>
               </Link>
             </div>
             <div className="nav-item">
-              <Link to='/create'>
+              <Link style={{textDecoration: 'none'}} to='/create'>
                 <p>Create</p>
               </Link>
             </div>
           </div>
           <div className="nav-item">
-              <Link to='/login'>
+              <Link style={{textDecoration: 'none'}} to='/login'>
                 <p>Login</p>
               </Link>
           </div>
