c36bc33a82776280eb4aa7e29f31e8c3510d94d7
hoangpham12345 <phamhuyhoang93tn@gmail.com>
2021-04-23 11:53:19 +0700

    Add some APIs in users

--- routes/api/users.js
+++ routes/api/users.js
@@ -25,12 +25,49 @@ user_router.get('/all', (req, res) => {
 // Get info of specific user_id (assume user_id is int)
 user_router.get('/:user_id', (req, res) => {
 
-    let user_id = parseInt(req.params.user_id)
+    let user_id = parseInt(req.params.user_id);
 
     connection.query(`SELECT * FROM users WHERE user_id = ${user_id}`, (err, rows, fields) => {
-        if (err) console.log(err)
+        if (err) console.log(err);
 
         res.json(rows);
+    })
+});
+
+// Insert new user
+user_router.post('/', (req, res) => {
+    let email = req.body.email;
+    let username = req.body.username;
+    let user_token = req.body.user_token;
+
+    connection.query(`INSERT INTO users (email, username, user_token) VALUES ("${email}", "${username}", "${user_token}")`, (err, rows, fields) => {
+        if (err) console.log(err);
+        else res.send("Succeed.")
+    })
+});
+
+// Update user (not really used)
+user_router.put('/:user_id', (req, res) => {
+    let user_id = parseInt(req.params.user_id);
+    let email = req.body.email;
+    let username = req.body.username;
+    let user_token = req.body.user_token; 
+
+    connection.query(`UPDATE users SET email = "${email}", username = "${username}", user_token = "${user_token}" WHERE user_id = "${user_id}"`, (err, rows, fields) => {
+        if (err) console.log(err);
+        else res.send("Succeed.")
+    })
+});
+
+// Delete user
+user_router.delete('/:user_id', (req, res) => {
+
+    let user_id = parseInt(req.params.user_id);
+
+    connection.query(`DELETE FROM users WHERE user_id = "${user_id}"`, (err, rows, fields) => {
+        if (err) console.log(err);
+
+        res.send("Success");
     })
 });
 
