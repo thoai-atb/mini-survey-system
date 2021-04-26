eecc628b51e3aea8645bc660484114d7cfd550e6
Thoai Ly <lybaothoai@gmail.com>
2021-04-26 11:23:34 +0700

    Display username on nav bar

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -9,7 +9,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
-    const [currentUserID] = useState() // ID in minisurvey database
+    const [currentUserID, setCurrentUserID] = useState() // ID in minisurvey database
     const [loading, setLoading] = useState(true)
 
     function signup(email, password) {
@@ -36,6 +36,16 @@ export function AuthProvider({ children }) {
         return currentUser.updatePassword(password)
     }
 
+    /////////////////////////////////////////////////////// DATABASE FETCHES
+
+    async function getUserName () {
+        if(!currentUserID)
+            return "Not Logged In"
+        const res = await fetch(`/api/users/${currentUserID}`)
+        const data = await res.json()
+        return data[0].username
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
@@ -43,9 +53,27 @@ export function AuthProvider({ children }) {
         })
         return unsubscribe
     }, [])
-
+    
     useEffect(() => {
-        // setCurrentUserID here
+        const fetchUserID = async () => {
+            let res = await fetch(`/api/users/tokentoid/${currentUser.uid}`)
+            /*
+                If currentUser changes due to signin up, then the user ID probably won't be available
+                because the back-end is creating the new user.
+                Therefore we need to create a loop to constantly check if it is present.
+            */
+            while (res.status !== 200) {
+                console.log('Could not get user ID, refetching...')
+                await new Promise(r => setTimeout(r, 100))
+                res = await fetch(`/api/users/tokentoid/${currentUser.uid}`)
+                if (res.status === 200)
+                    console.log('User ID found')
+            }
+            const data = await res.json()
+            setCurrentUserID(data.user_id)
+        }
+        if (currentUser)
+            fetchUserID()
     }, [currentUser])
 
     const value = {
@@ -57,6 +85,7 @@ export function AuthProvider({ children }) {
         resetPassword,
         updateEmail,
         updatePassword,
+        getUserName,
     }
 
     return (
