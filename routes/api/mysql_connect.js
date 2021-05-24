1702e7c5ae0649ff4464e0f6d0de93bf4dddcabc
hoangpham12345 <phamhuyhoang93tn@gmail.com>
2021-05-24 10:22:38 +0700

    Fixxed mistyping in DB connection

--- routes/api/mysql_connect.js
+++ routes/api/mysql_connect.js
@@ -12,7 +12,7 @@ var pool = mysql.createPool({
     connectionLimit: 10, 
     host: 'localhost',
     user: 'root',
-    passwors: '',
+    password: '',
     database: 'mini_survey_system',
 });
 
