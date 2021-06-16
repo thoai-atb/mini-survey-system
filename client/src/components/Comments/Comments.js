d910d2137911e3a7602d4bd9f0d8a8f932c00000
sonthanhngo <ngothanhson123456@gmail.com>
2021-06-16 22:32:02 +0700

    fix css for comment (but still a crap)

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -45,18 +45,20 @@ export default function Comments({survey}) {
     return(
         <div className=''>
             <h3>{comments.length} comments</h3>
-            <div className = 'txt-ctr'>  
+            <div className = 'comment-area'>  
             {
                 comments.map((comment, id) => {
                     return <Comment key={id} comment={comment}/>
                 })
             }
             </div>
+            <h3>Your Comment here!</h3>
             {
-            currentUserID === null ? <h3>Login to comment</h3> :
-                <div className = "" >
-                    <h3>Your Comment here!</h3>
-                    <input type ="textarea" ref={commentRef} className="" placeholder="Write a comment..."/>
+            currentUserID === null ? 
+                <h3>Login to comment</h3> 
+                :
+                <div className = "comment-input-area" >
+                    <input type ="textarea" rows = "10" cols="60" ref={commentRef} className="comment-box" placeholder="Write a comment..."/>
                     <input type ="button" className="add-comment-btn" value="Add" onClick={submitComment}/>
                 </div>
             }
