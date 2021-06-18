440d5ca125340ad974b143607bd0d91f1668b41c
Thoai Ly <lybaothoai@gmail.com>
2021-06-18 11:54:37 +0700

    add mock edit and delete for comment

--- client/src/components/Comments/Comment.js
+++ client/src/components/Comments/Comment.js
@@ -6,6 +6,20 @@ import formatDate from '../../utils/DateFormat'
 
 
 export default function Comment({comment}){
+    
+    const editComment = () => {
+        let editedComment = window.prompt("Edit Your Comment: ")
+        console.log(editedComment)
+    }
+
+    const deleteComment = () => {
+        if(window.confirm("Are you sure you want to delete this comment?"))
+            console.log("comment deleted");
+        else
+            console.log("comment deletion cancelled")
+    }
+
+
     return(
         <div className ='card card-wide comment-card' >
             <div className='comment-header'>
@@ -13,6 +27,10 @@ export default function Comment({comment}){
                 <div className ='comment-date'>{formatDate(new Date(comment.time))}</div>
             </div>
             <div className ='comment-content'>{comment.content}</div>
+            <div className='comment-footer'>
+                <span className="comment-modify-button" onClick={() => editComment()}>Edit</span>
+                <span className="comment-modify-button" onClick={() => deleteComment()}>Delete</span>
+            </div>
         </div>
     )
 }
